40e41ccb4d6d80c4e40d2efc4dd8358ef8cda585
Loren Goodwin <18320375+duckdotapk@users.noreply.github.com>
2025-10-22 20:17:56 -0400

    feat: add SetStageMusicEvent

--- src/Game.lua
+++ src/Game.lua
@@ -616,6 +616,7 @@ local ASFCommands = {
 	{ Name = "SetStageDynaLoadData", MinArgs = 1, MaxArgs = 2, RequiresScope = "Stage" },
 	{ Name = "SetStageEnabledCheats", MinArgs = 1, MaxArgs = 15, RequiresScope = "Stage" },
 	{ Name = "SetStageKickForce", MinArgs = 1, MaxArgs = 1, RequiresScope = "Stage" },
+	{ Name = "SetStageMusicEvent", MinArgs = 1, MaxArgs = 1, RequiresScope = "Stage" },
 	{ Name = "SetStagePedGroup", MinArgs = 1, MaxArgs = 1, RequiresScope = "Stage" },
 	{ Name = "SetStageSlamForce", MinArgs = 1, MaxArgs = 1, RequiresScope = "Stage" },
 	{ Name = "SetStageTrafficGroup", MinArgs = 1, MaxArgs = 1, RequiresScope = "Stage" },
